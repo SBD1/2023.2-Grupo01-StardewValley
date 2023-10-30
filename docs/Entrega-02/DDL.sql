a74ca789cb3d9d7f961a9f60fadeb0cee83817b6
Zenilda Pedrosa Vieira <zenilda.vieira@aluno.unb.br>
2023-10-30 20:29:30 -0300

    atualizacao versao final DDL, DML, DQL
    co-authored-by: Edilberto Cantuaria <edilbertounbfga@gmail.com>

--- docs/Entrega-02/DDL.sql
+++ docs/Entrega-02/DDL.sql
@@ -11,6 +11,7 @@
 --                            | Descricao: Correção das linhas de CREATE TABLE  |      --
 -- --------------------------------------------------------------------------------------
 
+BEGIN TRANSACTION; 
 
 CREATE TABLE Estacao (
     id_estacao SERIAL PRIMARY KEY,
@@ -18,14 +19,26 @@ CREATE TABLE Estacao (
     descricao char(800)
 );
 
+CREATE TABLE Tipo_Item (
+    id_tipo_item SERIAL PRIMARY KEY,
+    nome_tipo_item char(50) NOT NULL
+);
+
 CREATE TABLE Item (
     id_item SERIAL PRIMARY KEY,
-    id_tipo char(50) NOT NULL
+    id_tipo_item int NOT NULL,
+    FOREIGN KEY (id_item) REFERENCES Tipo_Item (id_tipo_item)
+);
+
+CREATE TABLE Tipo_Local_Fechado (
+    id_tipo_local_fechado SERIAL PRIMARY KEY,
+    nome_tipo_local_fechado char(50) NOT NULL
 );
 
 CREATE TABLE Local_Fechado (
     id_local_fechado SERIAL PRIMARY KEY,
-    id_tipo char(50) NOT NULL
+    id_tipo_local_fechado char(50) NOT NULL,
+    FOREIGN KEY (id_local_fechado) REFERENCES Tipo_Local_Fechado (id_tipo_local_fechado)
 );
 
 CREATE TABLE Mundo (
@@ -58,10 +71,10 @@ CREATE TABLE Consumivel (
 
 CREATE TABLE Efeito (
     id_efeito int NOT NULL PRIMARY KEY,
-    item int NOT NULL,
+    id_item int NOT NULL,
     valor int NOT NULL,
     tipo char(100),
-    FOREIGN KEY (item) REFERENCES Item (id_item)
+    FOREIGN KEY (id_item) REFERENCES Item (id_item)
 );
 
 CREATE TABLE Ferramenta (
@@ -81,33 +94,40 @@ CREATE TABLE Vestimenta (
 
 CREATE TABLE Monstro (
     id_monstro SERIAL PRIMARY KEY,
-    item_drop int NOT NULL,
+    id_drop int NOT NULL,
     nome char(50) NOT NULL,
     descricao char(800),
     dano int NOT NULL,
     defesa int NOT NULL,
     saude_maxima int NOT NULL,
-    FOREIGN KEY (item_drop) REFERENCES Item (id_item)
+    FOREIGN KEY (id_drop) REFERENCES Item (id_item)
 );
 
-CREATE TABLE Semente (
-    id_semente SERIAL PRIMARY KEY,
-    estacao int NOT NULL,
+CREATE TABLE Informacao_Semente (
+    id_info_semente SERIAL PRIMARY KEY,
+    id_semente int NOT NULL,
     nome char(50) NOT NULL,
     descricao char(800),
     valor_venda int NOT NULL,
     dias_para_crescer int NOT NULL,
-    FOREIGN KEY (id_semente) REFERENCES Item (id_item),
-    FOREIGN KEY (estacao) REFERENCES Estacao (id_estacao)
+    FOREIGN KEY (id_semente) REFERENCES Item (id_item)
 );
 
+CREATE TABLE Semente (
+    id_semente SERIAL PRIMARY KEY,
+    id_info_semente int NOT NULL,
+    id_estacao int NOT NULL,
+    FOREIGN KEY (id_semente) REFERENCES Item (id_item),
+    FOREIGN KEY (id_info_semente) REFERENCES Informacao_Semente (id_info_semente),
+    FOREIGN KEY (id_estacao) REFERENCES Estacao (id_estacao)
+);
 
 CREATE TABLE Missao (
     id_missao SERIAL PRIMARY KEY,
-    estacao int NOT NULL,
+    id_estacao int NOT NULL,
     nome char(50) NOT NULL,
     descricao char(800),
-    FOREIGN KEY (estacao) REFERENCES Estacao (id_estacao)
+    FOREIGN KEY (id_estacao) REFERENCES Estacao (id_estacao)
 );
 
 CREATE TABLE Regiao (
@@ -119,120 +139,123 @@ CREATE TABLE Regiao (
 
 CREATE TABLE NPC (
     id_npc SERIAL PRIMARY KEY,
-    regiao int NOT NULL,
-    local_NPC int NOT NULL,
+    id_regiao int NOT NULL,
+    id_local_fechado int NOT NULL,
     nome char(50) NOT NULL,
     profissao char(100),
-    FOREIGN KEY (regiao) REFERENCES Regiao (id_regiao),
-    FOREIGN KEY (local_NPC) REFERENCES Local_Fechado (id_local_fechado)
+    FOREIGN KEY (id_regiao) REFERENCES Regiao (id_regiao),
+    FOREIGN KEY (id_local_fechado) REFERENCES Local_Fechado (id_local_fechado)
 );
 
 CREATE TABLE Caverna (
-    id_caverna SERIAL PRIMARY KEY,
-    regiao int NOT NULL,
+    id_local_fechado SERIAL PRIMARY KEY,
+    id_regiao int NOT NULL,
     nome char(50) NOT NULL,
     descricao char(800),
-    FOREIGN KEY (id_caverna) REFERENCES Local_Fechado  (id_local_fechado),
-    FOREIGN KEY (regiao) REFERENCES Regiao (id_regiao)
+    FOREIGN KEY (id_local_fechado) REFERENCES Local_Fechado  (id_local_fechado),
+    FOREIGN KEY (id_regiao) REFERENCES Regiao (id_regiao)
 );
 
 
 CREATE TABLE Cabana_NPC (
-    id_cabana_npc SERIAL PRIMARY KEY,
-    npc int NOT NULL,
-    regiao int NOT NULL,
+    id_local_fechado SERIAL PRIMARY KEY,
+    id_npc int NOT NULL,
+    id_regiao int NOT NULL,
     nome char(50) NOT NULL,
     descricao char(800),
-    FOREIGN KEY (id_cabana_npc) REFERENCES Local_Fechado (id_local_fechado),
-    FOREIGN KEY (npc) REFERENCES NPC (id_npc),
-    FOREIGN KEY (regiao) REFERENCES Regiao (id_regiao)
+    FOREIGN KEY (id_local_fechado) REFERENCES Local_Fechado (id_local_fechado),
+    FOREIGN KEY (id_npc) REFERENCES NPC (id_npc),
+    FOREIGN KEY (id_regiao) REFERENCES Regiao (id_regiao)
 );
 
 CREATE TABLE Loja (
     id_loja SERIAL PRIMARY KEY,
-    proprietario int NOT NULL,
-    regiao int NOT NULL,
+    id_npc int NOT NULL,
+    id_regiao int NOT NULL,
     nome char(50) NOT NULL,
     descricao char(800),
     FOREIGN KEY (id_loja) REFERENCES Local_Fechado (id_local_fechado),
-    FOREIGN KEY (proprietario) REFERENCES NPC (id_npc),
-    FOREIGN KEY (regiao) REFERENCES Regiao (id_regiao)
+    FOREIGN KEY (id_npc) REFERENCES NPC (id_npc),
+    FOREIGN KEY (id_regiao) REFERENCES Regiao (id_regiao)
 );
 
 CREATE TABLE Jogador (
     id_jogador SERIAL PRIMARY KEY,
-    local_jogador int NOT NULL,
-    regiao int NOT NULL,
-    estacao_atual int NOT NULL,
-    missao_atual int,
+    id_local_fechado int NOT NULL,
+    id_regiao int NOT NULL,
+    id_estacao int NOT NULL,
+    id_missao int,
     nome char(50) NOT NULL,
     saude int NOT NULL,
     energia int NOT NULL,
     dia int NOT NULL,
+    hora int NOT NULL,
     qtdd_ouro int,
-    FOREIGN KEY (local_jogador) REFERENCES Local_Fechado (id_local_fechado),
-    FOREIGN KEY (regiao) REFERENCES Regiao (id_regiao),
-    FOREIGN KEY (estacao_atual) REFERENCES Estacao (id_estacao),
-    FOREIGN KEY (missao_atual) REFERENCES Missao (id_missao)
+    FOREIGN KEY (id_local_fechado) REFERENCES Local_Fechado (id_local_fechado),
+    FOREIGN KEY (id_regiao) REFERENCES Regiao (id_regiao),
+    FOREIGN KEY (id_estacao) REFERENCES Estacao (id_estacao),
+    FOREIGN KEY (id_missao) REFERENCES Missao (id_missao)
 );
 
 
 CREATE TABLE Item_Receita (
-    artesanato int NOT NULL,
-    item int NOT NULL,
+    id_artesanato int NOT NULL,
+    id_item int NOT NULL,
     qtdd int NOT NULL,
-    FOREIGN KEY (artesanato) REFERENCES Artesanato (id_artesanato),
-    FOREIGN KEY (item) REFERENCES Item (id_item)
+    FOREIGN KEY (id_artesanato) REFERENCES Artesanato (id_artesanato),
+    FOREIGN KEY (id_item) REFERENCES Item (id_item)
 );
 
 CREATE TABLE Item_Inventario (
-    jogador int NOT NULL,
-    item int NOT NULL,
+    id_jogador int NOT NULL,
+    id_item int NOT NULL,
     qtdd int NOT NULL,
-    FOREIGN KEY (jogador) REFERENCES Jogador (id_jogador),
-    FOREIGN KEY (item) REFERENCES Item (id_item)
+    FOREIGN KEY (id_jogador) REFERENCES Jogador (id_jogador),
+    FOREIGN KEY (id_item) REFERENCES Item (id_item)
 );
 
 CREATE TABLE Item_Estoque_Loja (
-    produto int NOT NULL,
-    loja int NOT NULL,
+    id_item int NOT NULL,
+    id_loja int NOT NULL,
     preco int NOT NULL,
-    FOREIGN KEY (produto) REFERENCES Item (id_item),
-    FOREIGN KEY (loja) REFERENCES Loja (id_loja)
+    FOREIGN KEY (id_item) REFERENCES Item (id_item),
+    FOREIGN KEY (id_loja) REFERENCES Loja (id_loja)
 );
 
 CREATE TABLE Instancia_Monstro (
-    caverna int NOT NULL,
-    monstro int NOT NULL,
+    id_caverna int NOT NULL,
+    id_monstro int NOT NULL,
     saude int NOT NULL,
-    FOREIGN KEY (caverna) REFERENCES Local_Fechado (id_local_fechado),
-    FOREIGN KEY (monstro) REFERENCES Monstro (id_monstro)
+    FOREIGN KEY (id_caverna) REFERENCES Local_Fechado (id_local_fechado),
+    FOREIGN KEY (id_monstro) REFERENCES Monstro (id_monstro)
 );
 
 CREATE TABLE Habilidade (
-    jogador int NOT NULL,
+    id_jogador int NOT NULL,
     nivel_coleta int NOT NULL,
     nivel_cultivo int NOT NULL,
     nivel_mineracao int NOT NULL,
     nivel_pesca int NOT NULL,
     nivel_combate int NOT NULL,
-    FOREIGN KEY (jogador) REFERENCES Jogador (id_jogador)
+    FOREIGN KEY (id_jogador) REFERENCES Jogador (id_jogador)
 );
 
 CREATE TABLE Dialogo (
     id_dialogo SERIAL PRIMARY KEY,
-    npc int NOT NULL,
+    id_npc int NOT NULL,
     fala char(800) NOT NULL,
-    FOREIGN KEY (npc) REFERENCES NPC (id_npc)
+    FOREIGN KEY (id_npc) REFERENCES NPC (id_npc)
 );
 
 CREATE TABLE Cabana_Jogador (
-    id_cabana_jog SERIAL PRIMARY KEY,
-    jogador int NOT NULL,
-    regiao int NOT NULL,
+    id_local_fechado SERIAL PRIMARY KEY,
+    id_jogador int NOT NULL,
+    id_regiao int NOT NULL,
     nome char(50) NOT NULL,
     descricao char(800),
-    FOREIGN KEY (id_cabana_jog) REFERENCES Local_Fechado (id_local_fechado),
-    FOREIGN KEY (jogador) REFERENCES Jogador (id_jogador),
-    FOREIGN KEY (regiao) REFERENCES Regiao (id_regiao)
+    FOREIGN KEY (id_local_fechado) REFERENCES Local_Fechado (id_local_fechado),
+    FOREIGN KEY (id_jogador) REFERENCES Jogador (id_jogador),
+    FOREIGN KEY (id_regiao) REFERENCES Regiao (id_regiao)
 );
+
+COMMIT;
--- docs/Entrega-02/DQL.sql
+++ docs/Entrega-02/DQL.sql
@@ -9,150 +9,150 @@
 --                            | Descricao: Inclusão das consultas do banco de dados |  --
 -- --------------------------------------------------------------------------------------
 
+BEGIN TRANSACTION;
+
 -- consultar o id e a descricao de uma estacao específica
-SELECT id_estacao, descricao FROM Estacao WHERE nome = 'Primavera';
+SELECT id_estacao, descricao 
+FROM Estacao 
+WHERE nome = 'Primavera';
 
 -- consultar o id de um tipo de item específico
-SELECT id_item FROM Item WHERE id_tipo = 'Semente';
+SELECT Item.id_item 
+FROM Item, Tipo_Item
+WHERE Item.id_tipo_item = Tipo_Item.id_tipo_item
+      and Tipo_Item.nome_tipo_item = 'Semente';
 
 -- consultar o id de um local fechado específico
-SELECT id_local_fechado FROM Local_Fechado WHERE id_tipo  = 'Loja';
+SELECT Local_Fechado.id_local_fechado
+FROM Local_Fechado, Tipo_Local_Fechado
+WHERE Local_Fechado.id_local_fechado = Tipo_Local_Fechado.id_tipo_local_fechado
+      and Tipo_Local_Fechado.nome_tipo_local_fechado  = 'Loja';
 
 -- consultar o id de um mundo específico
-SELECT id_mundo FROM Mundo WHERE nome = 'Fazenda';
+SELECT id_mundo 
+FROM Mundo 
+WHERE nome = 'Fazenda';
 
 -- consultar o id e a descricao de uma arma específica
-SELECT id_arma, descricao FROM Arma WHERE nome = 'espada';
+SELECT id_arma, descricao 
+FROM Arma 
+WHERE nome = 'espada';
 
 -- consultar a descricao, o dano e a finalidade de uma arma específica
-SELECT descricao, dano, finalidade FROM Arma WHERE id_arma = 1;
+SELECT descricao, dano, finalidade 
+FROM Arma 
+WHERE id_arma = 1;
 
 -- consultar o id de um artesanato específico
-SELECT id_artesanato, descricao FROM Artesanato WHERE nome = 'Espantalho';
+SELECT id_artesanato, descricao 
+FROM Artesanato 
+WHERE nome = 'Espantalho';
 
 -- consultar o id e a descricao de um consumivel específico
-SELECT id_consumivel, descricao FROM Consumivel WHERE nome = 'Omelete';
+SELECT id_consumivel, descricao 
+FROM Consumivel 
+WHERE nome = 'Omelete';
 
--- consultar o id e a descricao e o valor de um efeito específico
-SELECT id_efeito, descricao, valor FROM Efeito WHERE tipo = 'tipo1';
+-- consultar o id, valor e tipo de um efeito de um item específico 
+SELECT id_efeito, tipo, valor 
+FROM Efeito 
+WHERE  id_item = 2;
 
 -- consultar o id, a descricao e a finalidade de uma ferramenta específica
-SELECT id_ferramenta, descricao, finalidade FROM Ferramenta WHERE nome = 'enxada';
+SELECT id_ferramenta, descricao, finalidade 
+FROM Ferramenta 
+WHERE nome = 'enxada';
 
 -- consultar o id e a descricao de um vestimenta específico
-SELECT id_vestimenta, descricao FROM Vestimenta WHERE nome = 'Vestido de Baile de Inverno';
+SELECT id_vestimenta, descricao 
+FROM Vestimenta 
+WHERE nome = 'Vestido de Baile de Inverno';
 
 -- consultar o id e as informações de um monstro específico
-SELECT id_monstro, item_drop, dano, defesa, saude_maxima FROM Monstro WHERE nome = '';
+SELECT id_monstro, id_drop, dano, defesa, saude_maxima 
+FROM Monstro 
+WHERE nome = 'Serpente';
 
 -- consultar o id e as informações de uma semente específica
-SELECT id_semente, estacao, descricao, valor_venda, dias_para_crescer FROM Semente WHERE nome = 'Semente de Couve-flor';
+SELECT Semente.id_semente, id_estacao, descricao, valor_venda, dias_para_crescer 
+FROM Informacao_Semente, Semente 
+WHERE Informacao_Semente.nome = 'Semente de Couve-flor' 
+      and Semente.id_info_semente = Informacao_Semente.id_info_semente;
 
 -- consultar o id e as informações de uma missao específica
-SELECT id_missao, estacao, descricao FROM Missao WHERE nome = 'Plantando as Sementes';
+SELECT id_missao, id_estacao, descricao 
+FROM Missao 
+WHERE nome = 'Plantando as Sementes';
 
 -- consultar o id e o mundo de uma regiao específica
-SELECT id_regiao, mundo FROM Regiao WHERE nome = 'Vila Pelicanos';
+SELECT id_regiao, mundo 
+FROM Regiao 
+WHERE nome = 'Vila Pelicanos';
 
 -- consultar o id e as informações de um NPC específico
-SELECT id_npc, regiao, local_NPC, profissao FROM NPC WHERE nome = 'Willy';
+SELECT id_npc, id_regiao, id_local_fechado, profissao 
+FROM NPC 
+WHERE nome = 'Willy';
 
 -- consultar o id e as informações de uma caverna específica
-SELECT id_caverna, regiao, descricao FROM Caverna WHERE nome = 'Caverna das Minhocas';
+SELECT id_local_fechado, id_regiao, descricao 
+FROM Caverna 
+WHERE nome = 'Caverna das Minhocas';
 
 -- consultar o id e as informações de uma cabana_NPC específica
-SELECT id_cabana_npc, regiao, descricao FROM Cabana_NPC WHERE npc = 'Cabana de Robin';
+SELECT id_local_fechado, id_regiao, descricao 
+FROM Cabana_NPC 
+WHERE nome = 'Cabana de Robin';
 
 -- consultar o id e as informações de uma loja específica
-SELECT id_loja, proprietario, regiao, descricao FROM Loja WHERE nome = 'Loja de Linus';
-
--- Dados geral sobre o jogador
-SELECT regiao, estacao_atual, missao_atual, nome, saude, energia, qtdd_ouro FROM Jogador WHERE nome = ' Matheus';
+SELECT id_loja, id_npc, id_regiao, descricao 
+FROM Loja 
+WHERE nome = 'Loja de Linus';
 
 -- consultar o id e as informações de um jogador específico
-SELECT id_jogador, local_jogador, regiao, estacao_atual, missao_atual, saude, energia, dia, qtdd_ouro FROM Jogador WHERE nome = 'Marcus';
+SELECT id_jogador, id_local_fechado, id_regiao, id_estacao, id_missao, saude, energia, dia, hora, qtdd_ouro 
+FROM Jogador 
+WHERE nome = 'Marcus';
 
 -- consultar a receita de um artesanato específico
-SELECT item, qtdd FROM Item_Receita, Artesanato WHERE Item_Receita.artesanato = Artesanato.id_artesanato and Artesanato.nome = 'Tear';
+SELECT id_item, qtdd 
+FROM Item_Receita, Artesanato
+WHERE Item_Receita.id_artesanato = Artesanato.id_artesanato 
+      and Artesanato.nome = 'Tear';
 
 -- Consultar Habilidades
-SELECT nivel_coleta, nivel_cultivo, nivel_mineracao, nivel_pesca, nivel_combate FROM Habilidade, Jogador WHERE Habilidade.jogador = Jogador.id_jogador and Habilidade.jogador = 'Edilberto';
+SELECT nivel_coleta, nivel_cultivo, nivel_mineracao, nivel_pesca, nivel_combate 
+FROM Habilidade, Jogador 
+WHERE Habilidade.id_jogador = Jogador.id_jogador and Habilidade.id_jogador = 2;
 
 -- consultar o id e a fala de um dialogo específico
-SELECT id_dialogo, fala FROM Dialogo, NPC WHERE NPC.id_npc = Dialogo.npc and NPC.nome = 'Marlon';
+SELECT id_dialogo, fala 
+FROM Dialogo, NPC 
+WHERE NPC.id_npc = Dialogo.id_npc and NPC.nome = 'Marlon';
 
 -- consultar o id e as informações de uma cabana_jogador específica
-SELECT id_cabana_jog, regiao, nome, descricao FROM Cabana_Jogador, Jogador WHERE Cabana_Jogador.jogador = Jogador.id_jogador and Jogador.nome = 'Zenilda' 
-
-
--- Dados sobre o inventario do jogador
-SELECT I.nome, II.qtdd
-FROM Item_Inventario II
-JOIN Item I ON II.item = I.id_item
---WHERE II.jogador = ?;
-
--- Consultar a quantidade total de um tipo específico de item no inventário de um jogador:
-SELECT I.id_tipo, COUNT(*) as QuantidadeTotal
-FROM Item_Inventario II
-JOIN Item I ON II.item = I.id_item
---WHERE II.jogador = ? AND I.id_tipo = 'Arma'; -- ou qualquer outro tipo
+SELECT Cabana_Jogador.id_local_fechado, Cabana_Jogador.id_regiao, Cabana_Jogador.nome, Cabana_Jogador.descricao 
+FROM Cabana_Jogador, Jogador 
+WHERE Cabana_Jogador.id_jogador = Jogador.id_jogador and Jogador.nome = 'Zenilda';
 
 -- consultar o id de um item_inventario específico
-SELECT * FROM Item_Inventario (
-    jogador int NOT NULL,
-    item int NOT NULL,
-    qtdd int NOT NULL,
-    FOREIGN KEY (jogador) REFERENCES Jogador (id_jogador),
-    FOREIGN KEY (item) REFERENCES Item (id_item)
-);
+SELECT Item_Inventario.id_item, SUM(qtdd) as SomaQtdd
+FROM Item_Inventario, Item
+WHERE Item_Inventario.id_item = Item.id_item
+      and id_jogador = 3
+GROUP BY Item_Inventario.id_item;
 
--- Consulta Estoque de loja
-SELECT I.nome AS NomeDoItem, I.id_tipo AS TipoDoItem, I.id_item AS IDItem, I.descricao AS DescricaoDoItem, I.preco, I.qtdd AS QuantidadeEmEstoque
-FROM Item_Estoque_Loja IEL
-JOIN Item I ON IEL.produto = I.id_item
-WHERE IEL.loja = ?; -- Substituir ? pelo ID da loja desejada
+-- consultar o id e as informações de um item_estoque_loja específico
+SELECT Tipo_Item.nome_tipo_item, Item_Estoque_Loja.preco
+FROM Item_Estoque_Loja, Tipo_Item, Item
+WHERE Tipo_Item.id_tipo_item = Item.id_tipo_item  
+      and Item.id_item = Item_Estoque_Loja.id_item
+      and Item_Estoque_Loja.id_loja = 1;
 
--- consultar o id de um item_estoque_loja específico
-SELECT * FROM Item_Estoque_Loja (
-    produto int NOT NULL,
-    loja int NOT NULL,
-    preco int NOT NULL,
-    FOREIGN KEY (produto) REFERENCES Item (id_item),
-    FOREIGN KEY (loja) REFERENCES Loja (id_loja)
-);
+-- consultar o id e as informações de uma instância de monstro específica
+SELECT Instancia_Monstro.id_monstro, nome, descricao, id_drop, dano, defesa, saude_maxima, saude 
+FROM Monstro, Instancia_Monstro
+WHERE Monstro.id_monstro = Instancia_Monstro.id_monstro 
+      and Instancia_Monstro.id_caverna = 2;
 
-
--- Consulta de instancia de monstro
-SELECT M.id_monstro, M.nome AS NomeDoMonstro, M.descricao AS DescricaoDoMonstro, M.dano AS DanoDoMonstro, M.defesa AS DefesaDoMonstro, M.saude_maxima AS SaudeMaximaDoMonstro, IM.saude AS SaudeAtualDoMonstro
-FROM Monstro M
-JOIN Instancia_Monstro IM ON M.id_monstro = IM.monstro
-WHERE IM.id_local_fechado = ?; -- Substituir ? pelo ID da instância do monstro com o qual o jogador está lutando, necessário replicar a consulta para cada monstro
-
--- consultar o id de um instancia_monstro específico
-SELECT * FROM Instancia_Monstro (
-    caverna int NOT NULL,
-    monstro int NOT NULL,
-    saude int NOT NULL,
-    FOREIGN KEY (caverna) REFERENCES Local_Fechado (id_local_fechado),
-    FOREIGN KEY (monstro) REFERENCES Monstro (id_monstro)
-);
-
-
-
-
-
--- Consultas da tabela Jogador
-
--- Atualizar os atributos do jogador
--- atualizar local_jogador
--- atualizar regiao int NOT NULL,
--- atualizar estacao_atual int NOT NULL,
--- atualizar missao_atual int,
--- atualizar nome char(50) NOT NULL,
--- atualizar saude int NOT NULL,
--- atualizar energia int NOT NULL,
--- atualizar dia int NOT NULL,
--- atualizar qtdd_ouro int,
-
--- UPDATE Jogador SET local_jogador = 
-
+COMMIT;
