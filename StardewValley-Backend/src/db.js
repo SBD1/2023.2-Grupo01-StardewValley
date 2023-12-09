e403f8935c25b79e8cdb39768c6509373e9196d3
Matheus Silverio <matheusphillipo@gmail.com>
2023-12-09 02:19:02 -0300

    implementacao de arquitetura

---
+++ StardewValley-Backend/src/db.js
@@ -0,0 +1,27 @@
+import pg from "pg";
+
+const { Pool } = pg;
+
+async function connect() {
+  if (global.connection) return global.connection.connect();
+
+  console.log(`Connection string: ${process.env.CONNECTION_STRING}`);
+
+  const pool = new Pool({
+    connectionString: process.env.CONNECTION_STRING,
+  });
+
+  //apenas testando a conexão
+  const client = await pool.connect();
+  console.log("Criou pool de conexões no PostgreSQL!");
+
+  const res = await client.query("SELECT NOW()");
+  console.log(res.rows[0]);
+  client.release();
+
+  //guardando para usar sempre o mesmo
+  global.connection = pool;
+  return pool.connect();
+}
+
+export { connect };
