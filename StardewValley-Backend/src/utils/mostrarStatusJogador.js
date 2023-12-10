e3f40612b98141394d5e55c08fb52dad65cc6646
Matheus Silverio <matheusphillipo@gmail.com>
2023-12-10 11:09:23 -0300

    funcao MostrarStatusjogador

---
+++ StardewValley-Backend/src/utils/mostrarStatusJogador.js
@@ -0,0 +1,15 @@
+import { statusJogador } from "../services/status_jogador.js";
+
+export async function mostrarStatusJogador(jogadorId) {
+  const status = await statusJogador(jogadorId);
+
+  console.log("Status do jogador:");
+  console.log(`Nome: ${status.nome}`);
+  console.log(`Energia: ${status.energia}`);
+  console.log(`Ouro: ${status.qtdd_ouro}`);
+  console.log(`Pesca: ${status.nivel_pesca}`);
+  console.log(`Combate: ${status.nivel_combate}`);
+  console.log(`Coleta: ${status.nivel_coleta}`);
+  console.log(`Cultivo: ${status.nivel_cultivo}`);
+  console.log(`Mineração: ${status.nivel_mineracao}`);
+}
